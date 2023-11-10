077b07d0c273701a2ca817a0ce01f79daa731d9e
AditiGujar <adisushilgujar@gmail.com>
2023-11-10 09:16:35 -0600

    -scooter

--- Scooter.sql
+++ Scooter.sql
@@ -44,4 +44,29 @@ GROUP by 1,2
 ORDER by 3 ASC
 
 
+WITH latitude_count AS (SELECT ROUND(MAX(startlatitude),4)-.00005 AS rounded_lat,COUNT(startlatitude) AS start_lat,companyname
+FROM trips
+GROUP BY startlatitude,companyname
+ORDER BY start_lat DESC)
 
+SELECT DISTINCT rounded_lat,COUNT(rounded_lat) AS lat_count,companyname
+FROM latitude_count
+GROUP BY rounded_lat,companyname
+ORDER BY lat_count DESC;
+
+Select (DATE_PART('dow', '2019-05-19':: date ))
+from trips
+
+
+
+SELECT startdate, endlongitude, endlatitude,companyname,ROUND(MAX(startlatitude),4)-.00005 AS rounded_lat,COUNT(startlatitude) AS start_lat,
+CASE WHEN DATE_PART ('dow', startdate) = 1 THEN 'Monday'  -- (DATE_PART(dow) extract the Day of Week)
+	 WHEN DATE_PART ('dow', startdate) = 2 THEN 'Tuesday'
+	 WHEN DATE_PART ('dow', startdate) = 3 THEN 'Wednesday'
+	 WHEN DATE_PART ('dow', startdate) = 4 THEN 'Thursday'
+	 WHEN DATE_PART ('dow', startdate) = 5 THEN 'Friday'
+	 WHEN DATE_PART ('dow', startdate) = 6 THEN 'Saturday'
+	 WHEN DATE_PART ('dow', startdate) = 0 THEN 'Sunday' END  as days_of_week
+FROM trips
+GROUP BY 1,2,3,4
+ORDER BY start_lat DESC
